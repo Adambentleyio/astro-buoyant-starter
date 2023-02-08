58ee7eaae48972c711dc6d59a97412f3ada606ed
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2023-02-08 18:10:41 +0000

    changes to site titles and headings

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	title: `Buoyant Starter | A Marketing Website Theme for Therapists, Coaches, and Creators `,
+	description: `A simple, clean, and modern marketing website.`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
 	name: `Greyscale88`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
